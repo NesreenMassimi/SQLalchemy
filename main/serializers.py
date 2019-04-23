4e7357e70ecacc3c41ec0ec9c27c27b2499af5c0
NesreenMassimi <nmosimi@gmail.com>
2019-04-23 10:55:18 +0300

    change the strcuture

---
+++ main/serializers.py
@@ -0,0 +1,35 @@
+from marshmallow import fields
+from marshmallow_sqlalchemy import ModelSchema
+from .models import user,UserProfile,UserEducation
+
+
+class TokenSchema(ModelSchema):
+    class Meta :
+        model = user
+        fields=('token',)
+
+
+class UserEducationSchema(ModelSchema):
+    class Meta:
+        model = UserEducation
+
+
+class UserProfileSchema(ModelSchema):
+    class Meta :
+        model = UserProfile
+
+
+class UserSchema(ModelSchema):
+    profile = fields.Nested(UserProfileSchema())
+    educations = fields.Nested(UserEducationSchema,many=True)
+    class Meta :
+        model = user
+
+
+
+class UserUpdateSchema(ModelSchema):
+
+    class Meta :
+        model = user
+
+
