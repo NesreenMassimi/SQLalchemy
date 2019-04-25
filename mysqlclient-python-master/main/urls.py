17c7c17e5aadb0ed0a65084d761c0838f299bd27
NesreenMassimi <nmosimi@gmail.com>
2019-04-25 16:43:11 +0300

    mysql-client test

---
+++ mysqlclient-python-master/main/urls.py
@@ -0,0 +1,42 @@
+
+from django.urls import path
+from . import views
+
+details_user = views.Userview.as_view({
+
+    'put':'update',
+    'delete':'destroy',
+    'get' :'retrieve'
+
+
+})
+
+
+education = views.EducationView.as_view({
+
+    'post':'create',
+    'put':'update',
+    'delete':'destroy'
+
+
+
+
+})
+
+profile_details = views.ProfileView.as_view({
+
+    'put':'update'
+
+})
+
+urlpatterns = [ path('users', views.listUsers,name ='getUser'),
+                path('users/',views.createUser,name= "createuser"),
+                path('users/<int:pk>/profile',profile_details,name='update_profile'),
+                path('login/',views.log_in,name="login user"),
+                path('logout/', views.log_out, name="logout user"),
+                path('users/<int:pk>/',details_user,name="delete,update user"),
+                path('users/<int:pk>/education/',education,name='add_education'),
+                path('users/<int:pk>/education/<int:pk2>/',education,name="update_educauion")
+
+                ]
+
