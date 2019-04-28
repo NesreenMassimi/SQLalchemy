66469cb241b561018ceebfd45f8b3eeff2cc45bb
NesreenMassimi <nmosimi@gmail.com>
2019-04-28 22:48:28 +0300

    setting

--- SQLalchemy/settings.py
+++ SQLalchemy/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import pymysql
+#import pymysql
 from django.conf.global_settings import DATABASES
 
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
